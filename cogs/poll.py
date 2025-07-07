6973641d622add53512d6295bde45402fabfd0ca
Michael Mount (pc) <michaeljoesphmount@gmail.com>
2025-07-07 01:18:49 -0600

    built poll function

---
+++ cogs/poll.py
@@ -0,0 +1,20 @@
+# cogs/question.py
+import discord
+from discord.ext import commands
+
+class Poll(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+@commands.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def poll(ctx, *, question):
+    embed = discord.Embed(title="New Poll", description=question)
+    poll_message = await ctx.send(embed=embed)
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
+
+# Needed to load cog
+async def setup(bot):
+    await bot.add_cog(Poll(bot))
