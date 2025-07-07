aaa05b73368c2e3f3637f2f166f71116eaeb3182
Michael Mount (pc) <michaeljoesphmount@gmail.com>
2025-07-06 23:21:26 -0600

    Basic connection test to discord server

--- main.py
+++ main.py
@@ -9,4 +9,25 @@ import random
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-print(f"{token}")
+if not token:
+    raise ValueError("DISCORD_TOKEN isn't being passed form .env or is empty")
+
+## Log of errors and events
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+## The list of intents the Discord bot has from Discord App
+intents = discord.Intents.default()
+
+intents.message_content = True
+intents.members = True
+
+## Command Prefic trigger
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+## Basic Event to confrim Bot has connected to server
+@bot.event
+async def on_ready():
+    print(f"Howdy, {bot.user.name} is live!")
+
+## Start Command
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
